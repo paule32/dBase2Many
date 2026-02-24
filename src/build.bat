681b9393e7ca50228d694d81ed8c8c2ad0883f2f
paule32 <paule32.jk@gmail.com>
2026-02-24 15:02:42 +0100

    add locales support

--- src/build.bat
+++ src/build.bat
@@ -3,6 +3,30 @@
 :: \note (c) 2026 by Jens Kallup - paule32 aka Blacky Cat
 ::       all rights reserved.
 :: ---------------------------------------------------------------
+@echo off
+set "STARTDIR=%CD%"
+
+echo erstelle resources ...
 pyrcc5 resources.qrc -o resources_rc.py
-python build.py dBaseRunner.py -o start.py
-python -m compileall start.py
+::python build.py dBaseRunner.py -o start.py
+
+echo compile dBaseRunner.py ...
+python -m compileall dBaseRunner.py
+
+echo erstelle \en\LC_MESSAGES\dbase.mo ...
+pushd "locales\en\LC_MESSAGES"
+msgfmt -o dbase.mo dbase.po
+popd
+
+echo erstelle \de\LC_MESSAGES\dbase.mo ...
+pushd "locales\de\LC_MESSAGES"
+msgfmt -o dbase.mo dbase.po
+popd
+
+echo erstelle locales.zip
+pushd locales
+zip -9 -R locales.zip *.*
+copy /Y locales.zip ..\locales.zip
+popd
+
+echo fertig.
