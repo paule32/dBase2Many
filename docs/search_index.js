03eb572200c096b0785353e2285de2ca5ce99ac6
Blacky Cat <kallup-dev@web.de>
2026-06-06 15:26:03 +0200

    pascal docu engine html files
    - add pascal docu engine html demo files

---
+++ docs/search_index.js
@@ -0,0 +1,242 @@
+window.PASCAL_SEARCH_INDEX = [
+  {
+    "kind": "Class",
+    "name": "VirtualClass",
+    "text": "eine virtuelle Klasse",
+    "link": "VirtualClass.html"
+  },
+  {
+    "kind": "Class",
+    "name": "TBaseClass",
+    "text": "Basisklasse.",
+    "link": "TBaseClass.html"
+  },
+  {
+    "kind": "Class",
+    "name": "TChildClass",
+    "text": "Abgeleitete Klasse",
+    "link": "TChildClass.html"
+  },
+  {
+    "kind": "Class",
+    "name": "TExampleClass",
+    "text": "Beispielklasse.",
+    "link": "TExampleClass.html"
+  },
+  {
+    "kind": "Class",
+    "name": "TSprite",
+    "text": "Sprite class implementing IDrawable",
+    "link": "TSprite.html"
+  },
+  {
+    "kind": "Class",
+    "name": "TWeapon",
+    "text": "",
+    "link": "TWeapon.html"
+  },
+  {
+    "kind": "Class",
+    "name": "TInventory",
+    "text": "",
+    "link": "TInventory.html"
+  },
+  {
+    "kind": "Class",
+    "name": "TPlayer",
+    "text": "",
+    "link": "TPlayer.html"
+  },
+  {
+    "kind": "Class",
+    "name": "TPerson",
+    "text": "This is the TPerson class",
+    "link": "TPerson.html"
+  },
+  {
+    "kind": "Class",
+    "name": "TRepository<T>",
+    "text": "Generic repository class",
+    "link": "TRepository_T_.html"
+  },
+  {
+    "kind": "Class",
+    "name": "TVirtualClass",
+    "text": "Virtuelle Klasse",
+    "link": "TVirtualClass.html"
+  },
+  {
+    "kind": "Class",
+    "name": "TObject",
+    "text": "",
+    "link": "TObject.html"
+  },
+  {
+    "kind": "Class",
+    "name": "TFile",
+    "text": "",
+    "link": "TFile.html"
+  },
+  {
+    "kind": "Class",
+    "name": "TDialog",
+    "text": "",
+    "link": "TDialog.html"
+  },
+  {
+    "kind": "Class",
+    "name": "TWindows",
+    "text": "",
+    "link": "TWindows.html"
+  },
+  {
+    "kind": "Class",
+    "name": "TControl",
+    "text": "",
+    "link": "TControl.html"
+  },
+  {
+    "kind": "Class",
+    "name": "TCustomForm",
+    "text": "",
+    "link": "TCustomForm.html"
+  },
+  {
+    "kind": "Interface",
+    "name": "IExample",
+    "text": "Beispiel-Interface.",
+    "link": "IExample.html"
+  },
+  {
+    "kind": "Interface",
+    "name": "IDrawable",
+    "text": "Basis-Interface für zeichnbare Objekte",
+    "link": "IDrawable.html"
+  },
+  {
+    "kind": "Interface",
+    "name": "IShape",
+    "text": "Interface für geometrische Formen",
+    "link": "IShape.html"
+  },
+  {
+    "kind": "Interface",
+    "name": "IList<T>",
+    "text": "Generisches Listen-Interface",
+    "link": "IList_T_.html"
+  },
+  {
+    "kind": "Interface",
+    "name": "IRenderable",
+    "text": "",
+    "link": "IRenderable.html"
+  },
+  {
+    "kind": "Interface",
+    "name": "IList<T>",
+    "text": "Generic list interface",
+    "link": "IList_T_.html"
+  },
+  {
+    "kind": "Interface",
+    "name": "IVirtualInterface",
+    "text": "Virtuelles Interface",
+    "link": "IVirtualInterface.html"
+  },
+  {
+    "kind": "record",
+    "name": "TPoint",
+    "text": "Beispiel-Record.",
+    "link": "TPoint.html"
+  },
+  {
+    "kind": "record",
+    "name": "TPair<TKey, TValue>",
+    "text": "Generic key value pair record",
+    "link": "TPair_TKey__TValue_.html"
+  },
+  {
+    "kind": "record",
+    "name": "TVirtualRecord",
+    "text": "Virtueller Record",
+    "link": "TVirtualRecord.html"
+  },
+  {
+    "kind": "array",
+    "name": "TIntArray",
+    "text": "this is a integer array 0 .. 10",
+    "link": "TIntArray.html"
+  },
+  {
+    "kind": "set",
+    "name": "TCharSet",
+    "text": "a char set",
+    "link": "TCharSet.html"
+  },
+  {
+    "kind": "enum",
+    "name": "TCard",
+    "text": "available cards",
+    "link": "TCard.html"
+  },
+  {
+    "kind": "enum",
+    "name": "TColor",
+    "text": "available colors",
+    "link": "TColor.html"
+  },
+  {
+    "kind": "enum",
+    "name": "TVirtualEnum",
+    "text": "Virtuelles Enum",
+    "link": "TVirtualEnum.html"
+  },
+  {
+    "kind": "Constant",
+    "name": "MAX_COUNT",
+    "text": "Maximale Anzahl.",
+    "link": "index.html#const_MAX_COUNT"
+  },
+  {
+    "kind": "Constant",
+    "name": "PI_VALUE",
+    "text": "This is the documentation for the PI const",
+    "link": "index.html#const_PI_VALUE"
+  },
+  {
+    "kind": "Constant",
+    "name": "APP_NAME",
+    "text": "This is the application name",
+    "link": "index.html#const_APP_NAME"
+  },
+  {
+    "kind": "Constant",
+    "name": "APP_VERSION",
+    "text": "This is the Version",
+    "link": "index.html#const_APP_VERSION"
+  },
+  {
+    "kind": "Variable",
+    "name": "score_board",
+    "text": "point score board",
+    "link": "index.html#var_score_board"
+  },
+  {
+    "kind": "Variable",
+    "name": "score_text",
+    "text": "player name text",
+    "link": "index.html#var_score_text"
+  },
+  {
+    "kind": "Variable",
+    "name": "main_width",
+    "text": "",
+    "link": "index.html#var_main_width"
+  },
+  {
+    "kind": "Variable",
+    "name": "main_height",
+    "text": "",
+    "link": "index.html#var_main_height"
+  }
+];
